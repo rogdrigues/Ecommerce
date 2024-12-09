54a6a066beb40d4bf5b1a57ef0a993deb576a202
phong <phongnhhe163436@fpt.edu.vn>
2024-12-09 22:21:01 +0700

    feat(FE): update register form's design

--- src/router/routes/user-routes.tsx
+++ src/router/routes/user-routes.tsx
@@ -1,9 +1,26 @@
+import AboutPage from "@/pages/user/about";
+import BookPage from "@/pages/user/book";
+import HomePage from "@/pages/user/home";
 import UserLayout from "router/layouts/user-layout";
 
 const userRoutes = [
     {
         path: "/",
         element: <UserLayout />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: "/book",
+                element: <BookPage />
+            },
+            {
+                path: "/about",
+                element: <AboutPage />
+            }
+        ]
     }
 ]
 
