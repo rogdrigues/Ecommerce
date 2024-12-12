1ec4cddf12252bcf4d1a8f356eab8e1615332e9f
NguyenHungPhong <90857528+rogdrigues@users.noreply.github.com>
2024-12-12 17:59:25 +0700

    feat(FE): add export and import button

--- src/pages/admin/user/index.tsx
+++ src/pages/admin/user/index.tsx
@@ -3,7 +3,6 @@ import TableUser from "./_components/user.table";
 const ManageUserPage = () => {
     return (
         <div>
-            <h1>ManageUserPage Page</h1>
             <TableUser />
         </div>
     );
