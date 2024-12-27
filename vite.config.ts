c80e927d5e698ddb4c9fcc1d8a366778df2b3731
NguyenHungPhong <90857528+rogdrigues@users.noreply.github.com>
2024-12-27 18:14:03 +0700

    chore(FE): optimize Vite configuration for commonjs dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    include: ["react-csv", "react-image-gallery"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-csv/, /react-image-gallery/, /node_modules/],
+    },
+  },
   server: {
     port: 3000,
   },
